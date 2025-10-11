6a8f7f1519177d4523a54d3ea47a3a011e2ce71e
Victor Gonzalez <dazedbutamused@gmail.com>
2025-10-10 18:07:53 -0700

    noodlin with delete

--- src/app/dashboard/(event)/event-card.tsx
+++ src/app/dashboard/(event)/event-card.tsx
@@ -12,15 +12,20 @@ import React from 'react';
 import { Event, EventTag } from '@/db';
 import TagList from './tag-list';
 import EventDetails from './event-details';
-
+import { Circle, CircleCheck } from 'lucide-react';
 type EventCardProps = {
   event: Event;
   eventTags?: string[];
+  deleteSelect?: boolean;
 };
-
-const EventCard = ({ event, eventTags }: EventCardProps) => {
+const EventCard = ({ event, eventTags, deleteSelect = true }: EventCardProps) => {
   return (
-    <Card className="h-[15rem] m-2 shadow-sm">
+    <Card className="h-[15rem] m-2 shadow-sm relative">
+      {deleteSelect && (
+        <button aria-label='delete' onClick={(evt) => console.log(evt)} className="absolute top-1 right-1 opacity-25 cursor-pointer">
+          <Circle />
+        </button>
+      )}
       <CardHeader className="grid grid-cols-4 items-center">
         <CardTitle className="col-span-3 text-center">
           {/* {event.name} */}
