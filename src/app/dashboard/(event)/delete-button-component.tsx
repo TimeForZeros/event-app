323fa90d6314ba4e0a333ef172d9528c73b543eb
Victor Gonzalez <dazedbutamused@gmail.com>
2025-10-10 22:39:15 -0700

    nav for button group

--- src/app/dashboard/(event)/delete-button-component.tsx
+++ src/app/dashboard/(event)/delete-button-component.tsx
@@ -5,10 +5,10 @@ import useEvent from './store';
 const DeleteButtonComponent = () => {
   const store = useEvent();
   return store.enableDelete ? (
-    <>
+    <nav>
       <Button onClick={() => {}} variant="destructive">Confirm Delete</Button>
       <Button onClick={() => store.setEnableDelete(false)}>Cancel</Button>
-    </>
+    </nav>
   ) : (
     <Button onClick={() => store.setEnableDelete(true)} variant="destructive">
       DELETE
